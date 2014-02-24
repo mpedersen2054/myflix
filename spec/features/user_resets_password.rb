b76b20a6bf8ffd84e6c2d3d7f76716b07cd08a78
Matt Pedersen <mpedersen2054@gmail.com>
2014-02-24 17:03:10 -0600

    add feature spec for reset password

---
+++ spec/features/user_resets_password.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+feature "user resets password" do
+  scenario "user successfully resets password" do
+    alice = Fabricate(:user, password: "old_password")
+    visit sign_in_path
+    click_link "Forgot password?"
+    fill_in "Email Address", with: alice.email
+    click_button "Send Email"
+
+    open_email(alice.email)
+    current_email.click_link("Reset my password")
+
+    fill_in "New Password", with: "new_password"
+    click_button "Reset Password"
+
+    fill_in "Email Address", with: alice.email
+    fill_in "Password", with: "new_password"
+    click_button "Sign in"
+    expect(page).to have_content("Welcome, #{alice.full_name}")
+  end
+end
