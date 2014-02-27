837526955084b16f701ff226eecdbf05230fed98
Matt Pedersen <mpedersen2054@gmail.com>
2014-02-26 21:53:38 -0600

    add heroku mailgun variables in environment files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,16 +18,16 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { host: 'www.mattflix.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://mattflix.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
+    address:              ENV['MAILGUN_SMTP_SERVER'],
+    port:                 ENV['MAILGUN_SMTP_PORT'],
     domain:               'mattflix@herokuapp.com',
-    user_name:            ENV['gmail_username'],
-    password:             ENV['gmail_password'],
+    user_name:            ENV['MAILGUN_SMTP_LOGIN'],
+    password:             ENV['MAILGUN_SMTP_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
