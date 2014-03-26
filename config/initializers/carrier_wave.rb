264b1fcd66a61f882a303e75d58d22e2a375a23e
Matt Pedersen <mpedersen2054@gmail.com>
2014-03-25 23:01:23 -0500

    add config.fog_directory heroku env variable

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,7 +6,7 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
       :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],
     }
-    config.fog_directory  = 'name_of_directory'
+    config.fog_directory  = ENV['S3_BUCKET_NAME']
   else
     config.storage = :file
     config.enable_processing = Rails.env.development?
