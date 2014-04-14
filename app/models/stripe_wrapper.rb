20789928547784dfbeb2347aa0ad487a09e874fa
Matt Pedersen <mpedersen2054@gmail.com>
2014-04-14 18:15:33 -0500

    add customer subscription plan

--- app/models/stripe_wrapper.rb
+++ app/models/stripe_wrapper.rb
@@ -25,6 +25,33 @@ module StripeWrapper
     end
   end
 
+  class Customer
+    attr_reader :response, :error_message
+
+    def initialize(options={})
+      @response = options[:response]
+      @error_message = options[:error_message]
+    end
+
+    def self.create(options={})
+      StripeWrapper.set_api_key
+      begin
+        response = Stripe::Customer.create(
+          card: options[:card],
+          email: options[:user].email,
+          plan: "base"
+        )
+        new(response: response)
+      rescue Stripe::CardError => e
+        new(error_message: e.message)
+      end
+    end
+
+    def successful?
+      response.present?
+    end
+  end
+
   def self.set_api_key
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
   end
--- spec/services/user_signup_spec.rb
+++ spec/services/user_signup_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 describe UserSignup do
   describe "#sign_up" do
     context "with valid personal info and valid card" do
-      let(:charge) { double(:charge, successful?: true) }
+      let(:customer) { double(:customer, successful?: true) }
       before do
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
+        StripeWrapper::Customer.should_receive(:create).and_return(customer)
       end
       after do
         ActionMailer::Base.deliveries.clear
@@ -53,8 +53,8 @@ describe UserSignup do
 
     context "with valid personal info and declined card" do
       it "does not create a new user record" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined.")
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
+        customer = double(:customer, successful?: false, error_message: "Your card was declined.")
+        StripeWrapper::Customer.should_receive(:create).and_return(customer)
         UserSignup.new(Fabricate.build(:user)).sign_up('1231231', nil)
         expect(User.count).to eq(0)
       end
@@ -67,7 +67,7 @@ describe UserSignup do
       end
 
       it "does not charge the card" do
-        StripeWrapper::Charge.should_not_receive(:create)
+        StripeWrapper::Customer.should_not_receive(:create)
         UserSignup.new(User.new(email: "joe@example.com")).sign_up('1231231', nil)
       end
 
